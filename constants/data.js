22582e28b93a82a79ef677b8d7d81d472d202f73
JoshuaSmeda <joshua.smeda@xneelo.com>
2021-09-13 22:02:18 +0200

    Website setup

---
+++ constants/data.js
@@ -0,0 +1,124 @@
+const userData = {
+  githubUsername: "JoshuaSmeda",
+  name: "Joshua Smeda",
+  designation: "DevOps Engineer",
+  avatarUrl: "/avatar.png",
+  email: "smeda.josh14@gmail.com",
+  phone: "+27 796915438",
+  address: "Blouberg Rise, Cape Town, South Africa.",
+  projects: [
+    {
+      title: "Elastalert Hive Alerter",
+      link: "https://github.com/Nclose-ZA/elastalert_hive_alerter",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "TheHive SLA Monitor",
+      link: "https://github.com/JoshuaSmeda/thehive_sla_monitor",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a devops engineer that loves building products and web applications that impact thousands of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "SaaS - K3s",
+    //currentProjectUrl: "https://bro-check-my-site.co.za",
+  },
+  experience: [
+    {
+      title: "Hosting Platform Engineer",
+      company: "xneelo",
+      year: "2020",
+      companyLink: "https://xneelo.com",
+      desc: "Contributing to our flagship hosting platform - which is our biggest revenue generator which hosts over 1 million (that's right) websites!",
+    },
+    {
+      title: "DevOps Engineer",
+      company: "Nclose",
+      year: "2018",
+      companyLink: "https://nclose.com",
+      desc: "Contributed and developed a anomaly-based detection ingestion service, which consumed more than 100+ terabytes of security logs for threat analysis. Used by major South African fintech companies.",
+    },
+    {
+      title: "Hosting Engineer",
+      company: "1-Grid / Webafrica",
+      year: "2017",
+      companyLink: "https://1-grid.com",
+      desc: "Did all things web-hosting related. Started learning Linux... Need I say more!?",
+    },
+    {
+      title: "Network/VoIP Engineer",
+      company: "TenacIT",
+      year: "2019",
+      companyLink: "https://tenacit.net",
+      desc: "Worked with a lot of MikroTik products, great products! #notsponsored. Also spent countless hours analyzing pcap's, because VoIP over ADSL.",
+    },
+    {
+      title: "IT Administrator",
+      company: "Tristar Technologies",
+      year: "2016",
+      companyLink: "https://www.tristar.co.za/",
+      desc: "First job out of tertiary, learnt a lot. Basically ensured all our customers infrastructure was running in tip-top shape, and fix it if not!",
+    },
+    {
+	  title: "Graduation",
+      company: "CTU",
+      year: "2019",
+      companyLink: "https://ctutraining.ac.za",
+      desc: "Studied IT, went well. Eventually, started working during the day at Tristar Technologies, and studying here at night. #grind",
+    },
+    {
+      title: "High School",
+      company: "Milnerton High School, Milnerton",
+      year: "2015",
+      companyLink: "https://upes.co.in",
+      desc: "I barely survived with 70% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    //instagram: "https://instagram.com/maninthere",
+    //twitter: "https://twitter.com/mannupaaji",
+    linkedin: "https://linkedin.com/in/manuarora28",
+    github: "https://github.com/manuarora700",
+    //facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
